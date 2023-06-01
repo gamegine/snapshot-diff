22db6c889bbac02f4c632a9fb48d7a7f8eb62dc8
gamegine <gamegine@protonmail.com>
2023-06-01 11:09:32 +0000

    fix

--- snapshot-diff.go
+++ snapshot-diff.go
@@ -20,6 +20,7 @@ func main() {
 	}
 	if len(volumes) != 0 {
 		v := &volumes[0]
+		CacheDirPath := v.CacheDir()
 		err := v.UpdateSnapshotsList()
 		if err != nil {
 			fmt.Printf("Error UpdateSnapshotsList: %v\n", err)
@@ -28,7 +29,7 @@ func main() {
 		fmt.Printf("volume %s, %d snapshots\n", v.Name(), len(v.Snapshots))
 		if len(v.Snapshots) != 0 {
 			s := &v.Snapshots[0]
-			fmt.Printf("snapshot: %s \n\tpath: %s\n\tcache: %s\n", s.Name(), s.Path, s.CachePath(v.Name()))
+			fmt.Printf("snapshot: %s \n\tpath: %s\n\tcache: %s\n", s.Name(), s.Path, s.CacheFilePath(CacheDirPath))
 			err = s.Load()
 			if err != nil {
 				fmt.Printf("Error Snapshots.Load: %v\n", err)
@@ -47,8 +48,7 @@ func main() {
 				}
 
 			}
-			path := v.CacheDir()
-			err = s.SaveCache(s.CacheFilePath(path))
+			err = s.SaveCache(s.CacheFilePath(CacheDirPath))
 			if err != nil {
 				fmt.Println(err)
 			}
